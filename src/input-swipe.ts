3db74a2c5417f3632cf41aa8ad4c6e9bff360ef5
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-11-13 19:57:05 -0500

    track press

--- src/input-swipe.ts
+++ src/input-swipe.ts
@@ -1,18 +1,25 @@
-import { Manager, Swipe } from '@egjs/hammerjs';
+import { DIRECTION_VERTICAL, Manager, Press, Swipe } from '@egjs/hammerjs';
 
 class Swipes {
 	x = 0;
 
 	y = 0;
 
+	press = false;
+
 	constructor() {
 		const manager = new Manager(document.body);
+		manager.add(new Press());
 		manager.add(
 			new Swipe({
 				threshold: 30,
 				time: 300,
+				direction: DIRECTION_VERTICAL,
 			})
 		);
+		manager.on('press', () => {
+			this.press = true;
+		});
 		manager.on('swipe', (e) => {
 			this.x += e.deltaX;
 			this.y += e.deltaY;
@@ -22,6 +29,7 @@ class Swipes {
 	update() {
 		this.x = 0;
 		this.y = 0;
+		this.press = false;
 	}
 
 	isHorizontal() {
