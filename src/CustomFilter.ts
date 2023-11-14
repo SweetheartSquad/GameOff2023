7ffbf320fe0cf70dcb63e130734e76140116cc29
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-11-13 19:56:17 -0500

    custom filter accepts initial uniform values

--- src/CustomFilter.ts
+++ src/CustomFilter.ts
@@ -1,8 +1,11 @@
 import { Filter, utils } from 'pixi.js';
 
 export class CustomFilter<T extends utils.Dict<unknown>> extends Filter {
-	constructor(fragmentSource: string) {
-		super(undefined, fragmentSource);
+	constructor(
+		fragmentSource: ConstructorParameters<typeof Filter>[1],
+		uniforms?: T
+	) {
+		super(undefined, fragmentSource, uniforms);
 	}
 
 	override get uniforms(): T {
