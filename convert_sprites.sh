fe0d29021f4688637f75a8a3f0d6348e65855165
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-11-16 21:58:29 -0500

    split out legs from rest of passenger animation

--- convert_sprites.sh
+++ convert_sprites.sh
@@ -8,7 +8,9 @@ OUTPUT="./src/assets/textures";
 FILE=${1:-"*"};
 
 for f in ./asset_src/passenger*.aseprite;
-do "$ASEPRITE" $f -b --split-tags --filename-format "{path}/{title}_{tag}.{frame1}.{extension}" --save-as "$OUTPUT/$(basename $f .aseprite).png";
+do 
+"$ASEPRITE" $f -b --ignore-layer "legs" --split-tags --filename-format "{path}/{title}_{tag}.{frame1}.{extension}" --save-as "$OUTPUT/$(basename $f .aseprite).png";
+"$ASEPRITE" $f -b --tag "legs" --layer "legs/*" --split-tags --filename-format "{path}/{title}_{tag}.{frame1}.{extension}" --save-as "$OUTPUT/$(basename $f .aseprite).png";
 done
 # for f in ./src/assets/textures/passenger*.png;
 # do "node" --no-experimental-fetch ./optimize-image.js $f;
