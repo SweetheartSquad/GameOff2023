764002748821e5a26ad3162de6425054d1c6b929
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-11-16 20:43:15 -0500

    split y skybox

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -157,10 +157,10 @@ export class GameScene extends GameObject {
 		const skybox = new Skybox(
 			Cubemap.fromFaces({
 				posx: tex('skybox_posx'),
-				posy: tex('skybox_y'),
+				posy: tex('skybox_posy'),
 				posz: tex('skybox_posz'),
 				negx: tex('skybox_negx'),
-				negy: tex('skybox_y'),
+				negy: tex('skybox_negy'),
 				negz: tex('skybox_negz'),
 			})
 		);
