db9f8f11ac812497e84dba1fc047dbedf4781baa
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-11-18 11:18:19 -0500

    monkeypatch more files

--- monkeypatch.js
+++ monkeypatch.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const fs = require('fs');
 
-const pathPixi3d = path.resolve(
-	__dirname,
-	'node_modules/pixi3d/dist/esm/pixi7/pixi3d.js'
-);
+[
+	'browser/pixi3d.js',
+	'browser/pixi3d.min.js',
+	'cjs/pixi7/pixi3d.js',
+	'cjs/pixi7/pixi3d.min.js',
+	'esm/pixi7/pixi3d.js',
+	'esm/pixi7/pixi3d.min.js',
+].forEach((p) => {
+	const pathPixi3d = path.resolve(__dirname, 'node_modules/pixi3d/dist/', p);
 
-let pixi3d = fs.readFileSync(pathPixi3d, { encoding: 'utf-8' });
-pixi3d = pixi3d.replace(/float GAMMA = 2\.2/, 'float GAMMA = 1.0');
-fs.writeFileSync(pathPixi3d, pixi3d, { encoding: 'utf-8' });
+	let pixi3d = fs.readFileSync(pathPixi3d, { encoding: 'utf-8' });
+	pixi3d = pixi3d.replace(/float GAMMA = 2\.2/, 'float GAMMA = 1.0');
+	fs.writeFileSync(pathPixi3d, pixi3d, { encoding: 'utf-8' });
+});
