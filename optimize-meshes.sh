e299dc62fb3d7fe79d97733575c5d560dc3da54e
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-11-29 23:00:00 -0500

    optimize meshes further

--- optimize-meshes.sh
+++ optimize-meshes.sh
@@ -4,7 +4,7 @@ INPUT=$f
 OUTPUT="./src/assets/models/$(basename $f)"
 npx @gltf-transform/cli weld $INPUT $OUTPUT
 npx @gltf-transform/cli dedup $OUTPUT $OUTPUT
-npx @gltf-transform/cli quantize $OUTPUT $OUTPUT
-npx @gltf-transform/cli reorder $OUTPUT $OUTPUT
+npx @gltf-transform/cli simplify $OUTPUT $OUTPUT
+npx @gltf-transform/cli meshopt $OUTPUT $OUTPUT
 npx @gltf-transform/cli prune $OUTPUT $OUTPUT
 done
