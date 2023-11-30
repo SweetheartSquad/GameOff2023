acf43aaa3df12f11bc151d51dff52c8b57b2306b
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-11-30 13:57:16 -0500

    Update optimize-meshes.sh

--- optimize-meshes.sh
+++ optimize-meshes.sh
@@ -4,7 +4,9 @@ for f in ./asset_src/models/*.glb;
 do 
 INPUT=$f
 OUTPUT="./src/assets/models/$(basename $f)"
-npx @gltf-transform/cli dedup $OUTPUT $OUTPUT
+npx @gltf-transform/cli dedup $INPUT $OUTPUT
+npx @gltf-transform/cli weld $OUTPUT $OUTPUT
+npx @gltf-transform/cli simplify $OUTPUT $OUTPUT
 npx @gltf-transform/cli meshopt $OUTPUT $OUTPUT
 npx @gltf-transform/cli prune $OUTPUT $OUTPUT
 done
